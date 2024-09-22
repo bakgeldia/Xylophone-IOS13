b7d122e8607ac50ce3f614403bf642206bcfce87
Bakgeldi Alkhabay <bakgeldia@gmail.com>
2024-09-22 13:20:19 +0500

    iOS App Development Bootcamp | Section 8 | Xylophone App - imported AVFoundation - added function that plays sound when clicking on the button - button's opacity changes for 0.2 seconds, which makes an animation of click

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
     
+    @IBAction func keyPressed(_ sender: UIButton) {
+        guard let name = sender.currentTitle else { return }
+        playSound(name)
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2 ) {
+            sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(_ name: String) {
+        guard let url = Bundle.main.url(forResource: name, withExtension: "wav") else { return }
 
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
